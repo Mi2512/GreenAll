ac8f9ac9a85843342a965ceb9840e5caa808b168
Mi2512 <64696692+Mi2512@users.noreply.github.com>
2021-04-09 14:21:49 +0300

    Delete bot_handlers.py

--- bot_handlers.py
+++
@@ -1,7 +0,0 @@
-from db import users_db
-
-
-def send_welcome(message):
-    if not users_db.find_one({"chat_id": message.chat.id}):
-        users_db.insert_one({"chat_id": message.chat.id})
-        users_db.insert_too({"username": message.username})
